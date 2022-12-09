616b52f8d7de67d0d76bb2aa5b184f84802361ed
MOHSIN AAZZAB <96668073+MOHSINAAZZAB@users.noreply.github.com>
2022-12-09 13:31:55 +0100

    Update DBconnection.java

--- src/main/java/swiftway/DB_connection/DBconnection.java
+++ src/main/java/swiftway/DB_connection/DBconnection.java
@@ -4,6 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+public class DBconnection {
+      private static final String url="jdbc:mysql://127.0.0.1:3306/swiftway";
+      private static final String user="root";
+      private static final String pass="";
+      private static Connection connection;
+      public static Connection getConnection() {
+        try {
 
+            connection=DriverManager.getConnection(url, user, pass);
+            System.out.println("Connection Etablie au  base de donnees avec succes !!");
+
+        }catch (SQLException e) {
+
+            System.out.println("Pas de Connection avec la Base de Donnees !!");
+            e.printStackTrace();
+
+        }  
+        return connection;
+    }
+    }
 
 
